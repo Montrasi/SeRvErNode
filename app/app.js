a59f59147a9e0fa8fa6e9d91a481aecaf2535d49
Montrasi <montrasim523@gmail.com>
2019-05-17 08:11:15 +0000

    serverNode

--- app/app.js
+++ app/app.js
@@ -24,54 +24,62 @@ app.post('/login', function(req, res) {
 
     var user = req.body.user;
     var pass = req.body.pass;
-         
-    var con = Connection();   
-    
+    var state = 'ko';
+
+    var con = Connection();
+
     con.connect(function(err) {
-      if (err) throw err;
-      con.query('SELECT cognome FROM Users WHERE username = "' + user + '" AND password = "' + pass + '"', function (err, result, fields) {
-        if (err) throw err ;
-            console.log('WOOOOW FUNZICA LA LOGIN!! L"UTENTE ' + user + ' HA ESEGUITO L"ACCESSO!!');
-            res.send(true)
-      });
+        if (err) throw err;
+        con.query('SELECT cognome FROM Users WHERE username = "' + user + '" AND password = "' + pass + '"', function(err, result, fields) {
+            if (err) throw err;
+            if (result == '') {
+                //console.log('niente ' + result)
+                res.send(state);
+            } else {
+                console.log("WOOOOW FUNZICA LA LOGIN!! L'UTENTE " + user + " HA ESEGUITO L'ACCESSO!!");
+                console.log('cose ' + result)
+                state= 'ok'
+                res.send(state)
+            }
+        });
     });
 })
 
 
 app.post('/register', function(req, res) {
-    
+
     var name = req.body.name;
     var surname = req.body.surname;
     var email = req.body.email;
     var user = req.body.user;
     var pass = req.body.pass;
-      
-   
+
+
     var con = Connection();
-    
+
     con.connect(function(err) {
-      if (err) throw err;
-      con.query('INSERT INTO Users(nome, cognome, email, username, password) VALUES("' + name + '","' + surname + '","' + email + '","' + user + '","' + pass + '")', function (err, result, fields) {
         if (err) throw err;
-            console.log('WOOOOW FUNZICA IL REGISTER!!  L"UTENTE ' + user + ' HA ESEGUITO LA REGISTRAZIONE!!');
+        con.query('INSERT INTO Users(nome, cognome, email, username, password) VALUES("' + name + '","' + surname + '","' + email + '","' + user + '","' + pass + '")', function(err, result, fields) {
+            if (err) throw err;
+            console.log("WOOOOW FUNZICA IL REGISTER!!  L'UTENTE " + user + " HA ESEGUITO LA REGISTRAZIONE!!");
             res.send(true)
-      });
+        });
     });
 })
 
 
 
-function Connection(){
-    
+function Connection() {
+
     var con = mysql.createConnection({
-      host: 'sql2.freemysqlhosting.net',
-      user: 'sql2291452',
-      password: 'tY7%yQ6*',
-      database: 'sql2291452'
+        host: 'sql2.freemysqlhosting.net',
+        user: 'sql2291452',
+        password: 'tY7%yQ6*',
+        database: 'sql2291452'
     });
-    
+
     return con;
-    
+
 }
 
 
